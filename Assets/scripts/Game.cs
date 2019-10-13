1238ca6ea6c85d35004ed7c741019d78171cfbce
snick harrison <harrisonnick16@gmai.com>
2019-10-13 19:45:15 -0400

    spawn robots

---
+++ Assets/scripts/Game.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+
+    [SerializeField]
+    robotSpawn[] spawns;
+
+    public int enemiesLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initalize the singlton then calls the spawn robot function
+        singleton = this;
+        spawnRobots();
+    }
+    
+
+    private void spawnRobots()
+    {
+        //goes through each member in the arra y to call spawn robots
+        foreach(robotSpawn spawn in spawns)
+        {
+            spawn.spawnRobot();
+            enemiesLeft++;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/robotSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotSpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] robots;
+
+
+    private int timesSpawned;
+    private int healthBonus = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //creates a robot sets it's health and psoition
+    public void spawnRobot()
+    {
+        timesSpawned++;
+        healthBonus += 1 * timesSpawned;
+
+        GameObject robot = Instantiate(robots[Random.Range(0, robots.Length)]);
+
+        robot.transform.position = transform.position;
+        robot.GetComponent<Robot>().health += healthBonus;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
