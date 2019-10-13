e77fd36b0dc4753bdd686b427124eedc8b3b0eab
snick harrison <harrisonnick16@gmai.com>
2019-10-13 16:48:12 -0400

    adding pickups

---
+++ Assets/scripts/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public int type;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        
+        if (collider.gameObject.GetComponent<Player>()!= null && collider.gameObject.tag =="Player")
+        {
+            collider.gameObject.GetComponent<Player>().PickUpItem(type);
+            Destroy(gameObject);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -17,6 +17,76 @@ public class Player : MonoBehaviour
 
         gunEquipper = GetComponent<GunEquipper>();
     }
+    //add health
+    private void pickUpHealth()
+    {
+        health += 50;
+            if (health>200)
+        {
+            health = 200;
+        }
+    }
+
+    //add armor
+    private void pickUpArmor()
+    {
+        armor += 15;
+    }
+
+    //add assultrifle ammo
+    private void PickUpAssulAmmo()
+    {
+        ammo.AddAmmo(Constants.assultRifle, 50);
+    }
+
+
+    // add pistol ammo
+    private void PickUpPistolAmmo()
+    {
+        ammo.AddAmmo(Constants.pistol, 20);
+    }
+
+    // add shot gun ammo
+    private void PickUpShotgunAmmo()
+    {
+        ammo.AddAmmo(Constants.shotgun, 10);
+    }
+
+
+
+    public void PickUpItem(int pickupType)
+    {
+        switch(pickupType)
+        {
+            case Constants.PickUpArmor:
+                pickUpArmor();
+                break;
+
+            case Constants.PickUpHealth:
+                pickUpHealth();
+                break;
+
+            case Constants.PickUpAssultRiffleAmmo:
+                PickUpAssulAmmo();
+                break;
+
+            case Constants.PickUpPistolAmmo:
+                PickUpPistolAmmo();
+                break;
+
+            case Constants.PickUpShotGunAmmo:
+                PickUpShotgunAmmo();
+                break;
+
+            default:
+                Debug.LogError("Bad Pick Up type passes" + pickupType);
+                break;
+                
+
+        }
+    }
+
+
 
 
     public void takeDamage(int amount)
