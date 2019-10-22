43fa0e1de67e6fe4ba38ce0d3a6bc51e03c45280
snick harrison <harrisonnick16@gmai.com>
2019-10-22 16:29:52 -0400

    update the gameUI script

--- Assets/scripts/GameUI.cs
+++ Assets/scripts/GameUI.cs
@@ -13,11 +13,108 @@ public class GameUI : MonoBehaviour
     [SerializeField]
     Sprite blueReticle;
     [SerializeField]
-     Image reticle;
+    Image reticle;
+
+    [SerializeField]
+    private Text ammoText;
+    [SerializeField]
+    private Text healthText;
+    [SerializeField]
+    private Text armorText;
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Text pickupText;
+    [SerializeField]
+    private Text waveText;
+    [SerializeField]
+    private Text enemyText;
+    [SerializeField]
+    private Text waveClearText;
+    [SerializeField]
+    private Text newWaveText;
+    [SerializeField]
+    Player player;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        setArmorText(player.armor);
+        setHealthText(player.health);
+    }
+
+    public void setArmorText(int armor)
+    {
+        armorText.text = "Armor: " + armor;
+    }
+
+    public void setHealthText(int health)
+    {
+        healthText.text = "Health: " + health;
+    }
+
+    public void setScoreText(int score)
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void setAmmoText(int ammo)
+    {
+        ammoText.text = "Ammo: " + ammo;
+    }
+
+    public void setWaveText(int time)
+    {
+        newWaveText.text = "Next Wave: " + time;
+    }
+
+
+    public void setEnemyText(int enemies)
+    {
+        enemyText.text = "Enemies: " + enemies;
+    }
+
+
+    public void ShowWaveClearBonus ()
+    {
+        waveClearText.GetComponent<Text>().enabled = true;
+        StartCoroutine("hideWaveClearBonus");
+
+    }
+
+    IEnumerator hideWaveClearBonus()
+    {
+        yield return new WaitForSeconds(4);
+        waveClearText.GetComponent<Text>().enabled = false;
+    }
+
+    public void SetPickUpText(string text)
+    {
+        pickupText.GetComponent<Text>().enabled = true;
+        pickupText.text = text;
+        // Restart the contureine so it dosent have a bug of ending early
+        StopCoroutine("hidePickupText");
+        StartCoroutine("hidePickupText");
+    }
+
+
+    IEnumerator hidePickupText()
+    {
+        yield return new WaitForSeconds(4);
+        pickupText.GetComponent<Text>().enabled = false;
+    }
+
+    public void ShowNewWaveText()
+    {
+        StartCoroutine("hideNewWaveText");
+        newWaveText.GetComponent<Text>().enabled = true;
+    }
+
+    IEnumerator hideNewWaveText()
+    {
+        yield return new WaitForSeconds(4);
+        newWaveText.GetComponent<Text>().enabled = false;
     }
 
     public void UpdateReticle()
