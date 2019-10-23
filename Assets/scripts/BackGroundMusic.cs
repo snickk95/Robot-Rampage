65e0f62f9eae8bb9da9cc55b32b5444f91b0e557
snick harrison <harrisonnick16@gmai.com>
2019-10-22 20:43:51 -0400

    added background sounds

---
+++ Assets/scripts/BackGroundMusic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMusic : MonoBehaviour
+{
+
+     void Start()
+    {
+        DontDestroyOnLoad(gameObject);   
+    }
+
+
+}
